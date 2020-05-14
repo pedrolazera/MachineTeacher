40363582998208900dbdffdb802aa3d0a9d20413
pedrolazera <pedrolazera@gmail.com>
2020-05-13 21:30:06 -0300

    teacher novo

--- machine_teacher/Teachers/Experiment5Teacher.py
+++ machine_teacher/Teachers/Experiment5Teacher.py
@@ -8,6 +8,7 @@ class Experiment5Teacher(Experiment3Teacher):
 		increase_amount, time_left):
 		assert len(self.iters_t_fim) >= 3
 		# self.iters_t_fim[-1] # instante em que o ultimo treino terminou
+		# self.iters_t_ini[-1] # instante em que o ultimo treino começou
 		# self.iters_t_fim[-2] # instante em que o penultimo treino terminou
 		# self.iters_n[-1] # tamanho do ultimo teaching set
 		# self.iters_n[-2] # tamanho do penultimo teaching set
