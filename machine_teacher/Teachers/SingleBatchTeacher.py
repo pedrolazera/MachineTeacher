a84ecadaaf40d9f58e0d0c5a486bba5cd5942711
Sergio Filho <sfilhofreitas@gmail.com>
2020-08-11 15:19:51 -0300

    adicionando parametro no SingleBatchTeacher para utilizar apenas uma francao do dataset

--- machine_teacher/Teachers/SingleBatchTeacher.py
+++ machine_teacher/Teachers/SingleBatchTeacher.py
@@ -6,14 +6,16 @@ from sklearn import preprocessing
 class SingleBatchTeacher(Teacher):
 	name = "SingleBatchTeacher"
 
-	def __init__(self):
+	def __init__(self, frac_dataset = 1.0):
+		self.frac_dataset = frac_dataset
 		return
 
 	def start(self, X, y, time_left: float):
 		return self._start(X, y, time_left)
 
 	def get_first_examples(self, time_left: float) -> np.ndarray:
-		return self.ids
+		size = np.round(self.frac_dataset*len(self.y))
+		return self.ids[:int(size)]
 
 	def get_new_examples(self, test_ids,
 		test_labels, time_left: float) -> np.ndarray:
